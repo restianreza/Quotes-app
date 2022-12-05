ba82bcd8dc703e926948246b69a9a3139f0bbc1d
restianreza <114054889+restianreza@users.noreply.github.com>
2022-12-05 10:27:38 +0700

    push adapter Quotws

--- app/src/main/java/com/reza/if3b/Adapter/AdapterQuotes.java
+++ app/src/main/java/com/reza/if3b/Adapter/AdapterQuotes.java
@@ -1,14 +1,47 @@
 package com.reza.if3b.Adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.reza.if3b.Model.QuotesModel;
 import com.reza.if3b.R;
 
-public class AdapterQuotes {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdapterQuotes extends RecyclerView.Adapter<AdapterQuotes.ViewHolderQuotes>{
+    private List<QuotesModel> listQuotes = new ArrayList<>();
+    private Context ctx;
+
+    public AdapterQuotes(List<QuotesModel> listQuotes, Context ctx) {
+        this.listQuotes = listQuotes;
+        this.ctx = ctx;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderQuotes onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View varView = LayoutInflater.from(ctx).inflate(R.layout.item_quotes, parent, false);
+        return new ViewHolderQuotes(varView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderQuotes holder, int position) {
+        QuotesModel QM = listQuotes.get(position);
+        holder.tvQuotes.setText(QM.getText());
+        holder.tvAuthor.setText(QM.getAuthor());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listQuotes.size();
+    }
 
     public class ViewHolderQuotes extends RecyclerView.ViewHolder {
         TextView tvQuotes, tvAuthor;
